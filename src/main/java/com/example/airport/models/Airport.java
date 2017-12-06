e5edfcdb62cda89b863f9819a9e58dc38ac9d95e
capgeminiKoen <33454421+capgeminiKoen@users.noreply.github.com>
2017-12-06 09:20:17 +0100

    Initial commit

---
+++ src/main/java/com/example/airport/models/Airport.java
@@ -0,0 +1,12 @@
+package com.example.airport.models;
+
+import javax.persistence.Entity;
+
+/**
+ * @author kgriffio
+ * Airport class. Contains Planes and stuff.
+ */
+@Entity
+public class Airport {
+
+}
---
+++ src/main/java/com/example/airport/models/Plane.java
@@ -0,0 +1,12 @@
+package com.example.airport.models;
+
+
+import javax.persistence.Entity;
+
+/**
+ * Plane class
+ * @author kgriffio
+ */
+@Entity
+public class Plane {
+}
