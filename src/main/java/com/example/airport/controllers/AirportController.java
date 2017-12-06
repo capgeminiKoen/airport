5b1a56fcec3b71de029a3de652200e9f641fe878
capgeminiKoen <33454421+capgeminiKoen@users.noreply.github.com>
2017-12-06 09:40:27 +0100

    filled controllers

--- src/main/java/com/example/airport/controllers/AirportController.java
+++ src/main/java/com/example/airport/controllers/AirportController.java
@@ -1,8 +1,38 @@
 package com.example.airport.controllers;
 
+import com.example.airport.models.Airport;
+import com.example.airport.repositories.AirportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("api/airport/airports")
 public class AirportController {
+
+    @Autowired
+    private AirportRepository airportRepository;
+
+    /**
+     * Get all airports
+     * @return All airports
+     */
+    @RequestMapping(value = "all", method = RequestMethod.GET)
+    public Iterable<Airport> getAirports(){
+        return airportRepository.findAll();
+    }
+
+    /**
+     * Add an airport
+     * @param airport airport to add
+     * @return airport that was added.
+     */
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public Airport addAirport(Airport airport){
+        return airportRepository.save(airport);
+    }
 }
--- src/main/java/com/example/airport/controllers/PlaneController.java
+++ src/main/java/com/example/airport/controllers/PlaneController.java
@@ -1,7 +1,37 @@
 package com.example.airport.controllers;
 
+import com.example.airport.models.Airport;
+import com.example.airport.models.Plane;
+import com.example.airport.repositories.PlaneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/airport/planes")
 public class PlaneController {
+
+    @Autowired
+    PlaneRepository planeRepository;
+
+
+    /**
+     * Get all planes
+     * @return All planes in the repo
+     */
+    @RequestMapping(value = "all", method = RequestMethod.GET)
+    public Iterable<Plane> getPlanes(){
+        return planeRepository.findAll();
+    }
+
+    /**
+     * Add a plane to the repo
+     * @param plane Plane to add
+     * @return Plane that was added.
+     */
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public Plane addPlane(Plane plane){
+        return planeRepository.save(plane);
+    }
 }
